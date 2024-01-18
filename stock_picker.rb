92dc96753942c0eb0f68c9e00f81307050adff1e
Hoang Ha <SmithMerch2301@gmail.com>
2024-01-18 23:29:21 +0700

    Update version 2 of Project

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,28 +1,15 @@
-def stock_picker(array)
-  max_index = array.find_index(array.max)
-  min_index = array.find_index(array.min)
-  if min_index < max_index
-    return [min_index, max_index]
-  else
-    array_1 = array[0..max_index]
-    profit_1 = array.max - array_1.min
-
-    array_2 = array[(max_index+1)..(min_index-1)]
-    profit_2 = array_2.max - array_2.min
-
-    array_3 = array[min_index..-1]
-    profit_3 = array_3.max - array.min
-
-    profit = [profit_1, profit_2, profit_3]
-    profit_index = profit.find_index(profit.max)
-    if profit_index == 0
-      return [array.find_index(array_1.min), max_index]
-    elsif profit_index == 1
-      return [array.find_index(array_2.min), array.find_index(array_2.max)]
-    else
-      return [min_index, array.find_index(array_3.max)]
+def stock_picker(stocks)
+  bestArr = []
+  min = stocks[0] - stocks[1]
+  stocks.each_with_index do |day, index1|
+    stocks[stocks.index(day)..].each do |sellDay|
+      if day - sellDay < min
+        min = day - sellDay
+        bestArr = [index1, stocks.index(sellDay)]
+      end
     end
   end
-    
+  bestArr
 end
+
 p stock_picker([17,3,6,9,15,8,6,1,10])
